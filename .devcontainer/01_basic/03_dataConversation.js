bc1637185bd821e2a7e60077ffdf7d4002d88890
Aman Solanki <148065969+SolankiAman004@users.noreply.github.com>
2025-01-28 12:36:07 +0000

    data type eg

---
+++ .devcontainer/01_basic/03_dataConversation.js
@@ -0,0 +1,34 @@
+// let score = "33deff"   value is NAn
+// let score = null;      value is 0
+// let score = undefined     value is NAn
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "";
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+// value is 1 => true;
+// value is 0  => false;
+// value is hitest = true;
+// value is "" = False;
+
+// let someNumber = 456;
+
+// let newNumber = String(someNumber);
+//  console.log(someNumber);
+//  console.log(typeof (someNumber));
+
+
+// ******** Operations*******
+
+let newNumber = 
+
+
+
