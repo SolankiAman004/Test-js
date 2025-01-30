903e9cb250bbcfce62edd4183deea9979e7ba96b
Aman Solanki <148065969+SolankiAman004@users.noreply.github.com>
2025-01-30 11:04:11 +0000

    changes

---
+++ .devcontainer/01_basic/06_strings.js
@@ -0,0 +1,27 @@
+// let name = "Alina"
+// let myRepocount = 67
+
+// console.log(`My name is ${name} and my repo has ${myRepocount}`)
+
+// // another method 
+
+// const gameNew = new String(`hitesh`);  
+// const gameNews = new String(`The quick brown fox jumps over the lazy dog.`); 
+// console.log(gameNews.split('',5))
+
+// const url = ("https/alina30$.com")
+
+// url.replace('30$', 0);
+// console.log(url.replace('30$', 0));
+
+// console.log(gameNew.charAt(0));       // ✅ Corrected - Outputs 'h'
+// console.log(gameNew.indexOf('t'));    // ✅ Corrected - Outputs 2 (index of 't')
+// console.log(gameNew.lastIndexOf('h'));// ✅ Corrected - Outputs 5 (last occurrence of 'h')
+// console.log(gameNew.toUpperCase());   // ✅ Corrected - Outputs 'HITESH 
+// console.log(gameNew.charCodeAt(0)); 
+// console.log(gameNew.substring(0, 3));
+// console.log(gameNews);
+// console.log(gameNew.codePointAt(0,5))
+// console.log(gameNews.trim());
+console.log(gameNews.split('_ , !'))
+
---
+++ .devcontainer/01_basic/07_num.js
@@ -0,0 +1,23 @@
+//  const score = 500
+// console.log(score);
+
+// const balance =  new Number (400);
+// console.log(balance);
+// console.log(balance.toExponential(6));
+// console.log(balance.toFixed(5));
+// // console.log(balance.toString().length);
+
+// const basic = new Number (289.4889)
+// console.log(basic.toPrecision(4));
+
+const meraNumber = new Number (-4)
+// console.log(Math.abs(-4))
+// console.log(Math.random())
+// console.log(Math.ceil(269.9))
+// console.log(Math.floor(-6.90))
+// console.log(Math.round(-4.5))
+
+const min = 1;
+const max = 9;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
